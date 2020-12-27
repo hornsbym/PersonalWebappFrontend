6edeb6ec6173d3c03cf78bc8a0ffe7e8b4a3322d
Mitch Hornsby <hornsbym19@mail.wlu.edu>
2020-12-27 09:06:05 -0500

    Reconfigured React app to call express backend for fetching data.

--- src/aws_constants.js
+++ src/aws_constants.js
@@ -1,7 +1,7 @@
 const AWS_CONSTANTS = {
     pathToBucket: "https://games-hornsbym.s3.us-east-2.amazonaws.com",
-    getGameNamesApiUrl: "http://localhost:3010/getGameNames",
-    getGamePhotosByNameApiUrl: "http://localhost:3010/getGamePhotos",
+    getGameNamesApiUrl: "/getGameNames",
+    getGamePhotosByNameApiUrl: "/getGamePhotos",
     profilePicturePath: "https://website-resources-hornsbym.s3.us-east-2.amazonaws.com/profilePicture.jpg"
 }
 
